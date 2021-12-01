46f41888a3a9b9e20777d4562476e46347216a5a
Michael Musil <infi.pc@gmail.com>
2021-12-01 15:14:27 +0100

    working alt key

--- dist/runtime.js
+++ dist/runtime.js
@@ -2,16 +2,82 @@
 exports.__esModule = true;
 exports.register = void 0;
 var dataByFilename = {};
+var baseColor = "#e90139";
+var hoverColor = "#C70139";
 function register(input) {
     console.log(input);
     dataByFilename[input.filePath] = input;
 }
 exports.register = register;
+function buidLink(filePath, loc) {
+    return "vscode://file" + filePath + ":" + loc.start.line + ":" + (loc.start.column + 1);
+}
 var PADDING = 6;
+function rerenderLayer(found, isAltKey) {
+    if (isAltKey) {
+        document.body.style.cursor = 'pointer';
+    }
+    else {
+        document.body.style.cursor = 'default';
+    }
+    if (found.dataset && found.dataset.visprId) {
+        var _a = found.dataset.visprId.split("::"), filePath = _a[0], id = _a[1];
+        var data = dataByFilename[filePath];
+        var expData = data.expressions[id];
+        if (expData) {
+            var bbox = found.getBoundingClientRect();
+            var rect = document.createElement("div");
+            rect.style.position = "absolute";
+            rect.style.left = bbox.x - PADDING + "px";
+            rect.style.top = bbox.y - PADDING + "px";
+            rect.style.width = bbox.width + PADDING * 2 + "px";
+            rect.style.height = bbox.height + PADDING * 2 + "px";
+            rect.style.border = "2px solid " + baseColor;
+            rect.style.borderRadius = "8px";
+            if (isAltKey) {
+                rect.style.backgroundColor = "rgba(255, 0, 0, 0.1)";
+            }
+            var topPart = document.createElement("div");
+            topPart.style.position = "absolute";
+            topPart.style.display = "flex";
+            topPart.style.justifyContent = "center";
+            topPart.style.top = "-30px";
+            topPart.style.left = "0px";
+            topPart.style.width = "100%";
+            rect.appendChild(topPart);
+            var labelWrapper = document.createElement("div");
+            labelWrapper.style.padding = "2px 10px 10px 10px";
+            // labelWrapper.style.backgroundColor = "#00ff00";
+            labelWrapper.style.pointerEvents = "auto";
+            labelWrapper.id = "vispr-label-wrapper";
+            topPart.appendChild(labelWrapper);
+            var label = document.createElement("a");
+            label.href = buidLink(filePath, expData.loc);
+            // label.style.backgroundColor = "#ff0000";
+            label.style.color = "#fff";
+            label.style.fontSize = "12px";
+            label.style.fontWeight = "bold";
+            label.style.textAlign = "center";
+            label.style.padding = "2px 6px";
+            label.style.borderRadius = "4px";
+            label.style.fontFamily = "Helvetica, sans-serif, Arial";
+            label.innerText = expData.name;
+            label.id = "vispr-label";
+            labelWrapper.appendChild(label);
+            var el = document.getElementById("vispr-layer");
+            if (!el) {
+                throw new Error("no layer found");
+            }
+            el.innerHTML = "";
+            el.appendChild(rect);
+            // document.body.childNodes = [rect]
+        }
+    }
+}
 if (typeof window !== "undefined") {
     // add style tag to head
     var style = document.createElement("style");
-    style.innerHTML = "\n        #vispr-label {\n            cursor: pointer;\n            background-color: #ff0000;\n        }\n        #vispr-label:hover {\n            background-color: #aa3333;\n        }\n    ";
+    style.innerHTML = "\n        #vispr-label {\n            cursor: pointer;\n            background-color: " + baseColor + ";\n        }\n        #vispr-label:hover {\n            background-color: " + hoverColor + ";\n        }\n    ";
     document.head.appendChild(style);
     document.addEventListener("scroll", function () {
         // hide layers when scrolling
@@ -19,84 +85,56 @@ if (typeof window !== "undefined") {
         if (!el) {
             throw new Error("no layer found");
         }
+        currentElementRef_1 = null;
         el.innerHTML = "";
     });
     document.addEventListener("mouseover", function (e) {
-        if (e.target) {
-            console.log(e.target);
-            if (
-            // @ts-ignore
-            e.target.id == "vispr-label" ||
-                // @ts-ignore
-                e.target.id == "vispr-label-wrapper") {
+        var target = e.target;
+        if (target && target instanceof HTMLElement) {
+            if (target.id == "vispr-label" || target.id == "vispr-label-wrapper") {
                 return;
             }
-            // @ts-ignore
-            var found = e.target.closest("[data-vispr-id]");
-            // add bouding box to the layer
-            if (found) {
-                var _a = found.dataset.visprId.split("::"), filePath = _a[0], id = _a[1];
-                var data = dataByFilename[filePath];
-                var expData = data.expressions[id];
-                if (expData) {
-                    var bbox = found.getBoundingClientRect();
-                    var rect = document.createElement("div");
-                    rect.style.position = "absolute";
-                    rect.style.left = bbox.x - PADDING + "px";
-                    rect.style.top = bbox.y - PADDING + "px";
-                    rect.style.width = bbox.width + PADDING * 2 + "px";
-                    rect.style.height = bbox.height + PADDING * 2 + "px";
-                    rect.style.border = "2px solid #ff0000";
-                    rect.style.borderRadius = "8px";
-                    var topPart = document.createElement("div");
-                    topPart.style.position = "absolute";
-                    topPart.style.display = "flex";
-                    topPart.style.justifyContent = "center";
-                    topPart.style.top = "-30px";
-                    topPart.style.left = "0px";
-                    topPart.style.width = "100%";
-                    rect.appendChild(topPart);
-                    var labelWrapper = document.createElement("div");
-                    labelWrapper.style.padding = "4px 8px 8px 8px";
-                    // labelWrapper.style.backgroundColor = "#00ff00";
-                    labelWrapper.style.pointerEvents = "auto";
-                    labelWrapper.id = "vispr-label-wrapper";
-                    topPart.appendChild(labelWrapper);
-                    var label = document.createElement("div");
-                    // label.style.backgroundColor = "#ff0000";
-                    label.style.color = "#fff";
-                    label.style.fontSize = "12px";
-                    label.style.fontWeight = "bold";
-                    label.style.textAlign = "center";
-                    label.style.padding = "2px 6px";
-                    label.style.borderRadius = "4px";
-                    label.style.fontFamily = "Helvetica, sans-serif, Arial";
-                    label.innerText = expData.name;
-                    label.id = "vispr-label";
-                    labelWrapper.appendChild(label);
-                    var el = document.getElementById("vispr-layer");
-                    if (!el) {
-                        throw new Error("no layer found");
-                    }
-                    el.innerHTML = "";
-                    el.appendChild(rect);
-                    // document.body.childNodes = [rect]
-                }
+            var found = target.closest("[data-vispr-id]");
+            if (found && found instanceof HTMLElement) {
+                currentElementRef_1 = new WeakRef(found);
+                rerenderLayer(found, e.altKey);
             }
         }
     }, { capture: true });
+    var currentElementRef_1 = null;
+    document.addEventListener("keydown", function (e) {
+        if (currentElementRef_1) {
+            var el = currentElementRef_1.deref();
+            if (el) {
+                rerenderLayer(el, e.altKey);
+            }
+        }
+    });
+    document.addEventListener("keyup", function (e) {
+        if (currentElementRef_1) {
+            var el = currentElementRef_1.deref();
+            if (el) {
+                rerenderLayer(el, e.altKey);
+            }
+        }
+    });
     document.addEventListener("click", function (e) {
-        if (e.target) {
-            // @ts-ignore
-            var found = e.target.closest("[data-vispr-id]");
-            console.log(found);
+        if (!e.altKey) {
+            return;
+        }
+        var target = e.target;
+        if (target && target instanceof HTMLElement) {
+            var found = target.closest("[data-vispr-id]");
+            if (!found || !found.dataset || !found.dataset.visprId) {
+                return;
+            }
             var _a = found.dataset.visprId.split("::"), filePath = _a[0], id = _a[1];
             var data = dataByFilename[filePath];
             console.log(data);
             console.log();
             var exp = data.expressions[Number(id)];
             // window.location.href =
-            var link = "vscode://file" + filePath + ":" + exp.loc.start.line + ":" + (exp.loc.start.column + 1);
+            var link = buidLink(filePath, exp.loc);
             console.log(link);
             var win = window.open(link, "_blank");
         }
--- src/runtime.ts
+++ src/runtime.ts
@@ -1,11 +1,83 @@
 const dataByFilename: { [filename: string]: any } = {};
 
+const baseColor = "#e90139"
+const hoverColor = "#C70139"
+
 export function register(input: any) {
   console.log(input);
   dataByFilename[input.filePath] = input;
 }
 
+function buidLink(filePath: string, loc: any) {
+  return `vscode://file${filePath}:${loc.start.line}:${loc.start.column + 1}`;
+}
 const PADDING = 6;
+
+function rerenderLayer(found: HTMLElement, isAltKey: boolean) {
+  if (isAltKey) {
+      document.body.style.cursor = 'pointer';
+  } else {
+      document.body.style.cursor = 'default';
+  }
+  if (found.dataset && found.dataset.visprId) {
+    const [filePath, id] = found.dataset.visprId.split("::");
+    const data = dataByFilename[filePath];
+    const expData = data.expressions[id];
+    if (expData) {
+      const bbox = found.getBoundingClientRect();
+      const rect = document.createElement("div");
+      rect.style.position = "absolute";
+      rect.style.left = bbox.x - PADDING + "px";
+      rect.style.top = bbox.y - PADDING + "px";
+      rect.style.width = bbox.width + PADDING * 2 + "px";
+      rect.style.height = bbox.height + PADDING * 2 + "px";
+      rect.style.border = "2px solid " + baseColor;
+      rect.style.borderRadius = "8px";
+      if (isAltKey) {
+        rect.style.backgroundColor = "rgba(255, 0, 0, 0.1)";
+      }
+
+      const topPart = document.createElement("div");
+      topPart.style.position = "absolute";
+      topPart.style.display = "flex";
+      topPart.style.justifyContent = "center";
+      topPart.style.top = "-30px";
+      topPart.style.left = "0px";
+      topPart.style.width = "100%";
+      rect.appendChild(topPart);
+
+      const labelWrapper = document.createElement("div");
+      labelWrapper.style.padding = "2px 10px 10px 10px";
+      // labelWrapper.style.backgroundColor = "#00ff00";
+      labelWrapper.style.pointerEvents = "auto";
+      labelWrapper.id = "vispr-label-wrapper";
+      topPart.appendChild(labelWrapper);
+
+      const label = document.createElement("a");
+      label.href = buidLink(filePath, expData.loc);
+      // label.style.backgroundColor = "#ff0000";
+      label.style.color = "#fff";
+      label.style.fontSize = "12px";
+      label.style.fontWeight = "bold";
+      label.style.textAlign = "center";
+      label.style.padding = "2px 6px";
+      label.style.borderRadius = "4px";
+      label.style.fontFamily = "Helvetica, sans-serif, Arial";
+      label.innerText = expData.name;
+      label.id = "vispr-label";
+      labelWrapper.appendChild(label);
+
+      const el = document.getElementById("vispr-layer");
+      if (!el) {
+        throw new Error("no layer found");
+      }
+      el.innerHTML = "";
+      el.appendChild(rect);
+
+      // document.body.childNodes = [rect]
+    }
+  }
+}
 
 if (typeof window !== "undefined") {
   // add style tag to head
@@ -13,10 +85,10 @@ if (typeof window !== "undefined") {
   style.innerHTML = `
         #vispr-label {
             cursor: pointer;
-            background-color: #ff0000;
+            background-color: ${baseColor};
         }
         #vispr-label:hover {
-            background-color: #aa3333;
+            background-color: ${hoverColor};
         }
     `;
   document.head.appendChild(style);
@@ -27,99 +99,66 @@ if (typeof window !== "undefined") {
     if (!el) {
       throw new Error("no layer found");
     }
+    currentElementRef = null;
     el.innerHTML = "";
   });
 
   document.addEventListener(
     "mouseover",
-    function (e) {
-      if (e.target) {
-        console.log(e.target);
-        if (
-          // @ts-ignore
-          e.target.id == "vispr-label" ||
-          // @ts-ignore
-          e.target.id == "vispr-label-wrapper"
-        ) {
+    (e) => {
+      const target = e.target;
+      if (target && target instanceof HTMLElement) {
+        if (target.id == "vispr-label" || target.id == "vispr-label-wrapper") {
           return;
         }
-        // @ts-ignore
-        const found = e.target.closest("[data-vispr-id]");
-        // add bouding box to the layer
 
-        if (found) {
-          const [filePath, id] = found.dataset.visprId.split("::");
-          const data = dataByFilename[filePath];
-          const expData = data.expressions[id];
-          if (expData) {
-            const bbox = found.getBoundingClientRect();
-            const rect = document.createElement("div");
-            rect.style.position = "absolute";
-            rect.style.left = bbox.x - PADDING + "px";
-            rect.style.top = bbox.y - PADDING + "px";
-            rect.style.width = bbox.width + PADDING * 2 + "px";
-            rect.style.height = bbox.height + PADDING * 2 + "px";
-            rect.style.border = "2px solid #ff0000";
-            rect.style.borderRadius = "8px";
-
-            const topPart = document.createElement("div");
-            topPart.style.position = "absolute";
-            topPart.style.display = "flex";
-            topPart.style.justifyContent = "center";
-            topPart.style.top = "-30px";
-            topPart.style.left = "0px";
-            topPart.style.width = "100%";
-            rect.appendChild(topPart);
-
-            const labelWrapper = document.createElement("div");
-            labelWrapper.style.padding = "4px 8px 8px 8px";
-            // labelWrapper.style.backgroundColor = "#00ff00";
-            labelWrapper.style.pointerEvents = "auto";
-            labelWrapper.id = "vispr-label-wrapper";
-            topPart.appendChild(labelWrapper);
-
-            const label = document.createElement("div");
-            // label.style.backgroundColor = "#ff0000";
-            label.style.color = "#fff";
-            label.style.fontSize = "12px";
-            label.style.fontWeight = "bold";
-            label.style.textAlign = "center";
-            label.style.padding = "2px 6px";
-            label.style.borderRadius = "4px";
-            label.style.fontFamily = "Helvetica, sans-serif, Arial";
-            label.innerText = expData.name;
-            label.id = "vispr-label";
-            labelWrapper.appendChild(label);
-
-            const el = document.getElementById("vispr-layer");
-            if (!el) {
-              throw new Error("no layer found");
-            }
-            el.innerHTML = "";
-            el.appendChild(rect);
-
-            // document.body.childNodes = [rect]
-          }
+        const found = target.closest("[data-vispr-id]");
+        if (found && found instanceof HTMLElement) {
+          currentElementRef = new WeakRef(found);
+          rerenderLayer(found, e.altKey);
         }
       }
     },
     { capture: true }
   );
 
+  let currentElementRef: null | WeakRef<HTMLElement> = null;
+
+  document.addEventListener("keydown", (e) => {
+    if (currentElementRef) {
+      const el = currentElementRef.deref();
+      if (el) {
+        rerenderLayer(el, e.altKey);
+      }
+    }
+  });
+
+  document.addEventListener("keyup", (e) => {
+    if (currentElementRef) {
+      const el = currentElementRef.deref();
+      if (el) {
+        rerenderLayer(el, e.altKey);
+      }
+    }
+  });
+
   document.addEventListener("click", function (e) {
-    if (e.target) {
-      // @ts-ignore
-      const found = e.target.closest("[data-vispr-id]");
-      console.log(found);
+    if (!e.altKey) {
+      return;
+    }
+    const target = e.target;
+    if (target && target instanceof HTMLElement) {
+      const found: HTMLElement | null = target.closest("[data-vispr-id]");
+      if (!found || !found.dataset || !found.dataset.visprId) {
+        return;
+      }
       const [filePath, id] = found.dataset.visprId.split("::");
       const data = dataByFilename[filePath];
       console.log(data);
       console.log();
       const exp = data.expressions[Number(id)];
       // window.location.href =
-      const link = `vscode://file${filePath}:${exp.loc.start.line}:${
-        exp.loc.start.column + 1
-      }`;
+      const link = buidLink(filePath, exp.loc);
       console.log(link);
       var win = window.open(link, "_blank");
     }
