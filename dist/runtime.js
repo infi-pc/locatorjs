a6a95141b7191715593b4f0c9bb8aaeceed2b4d3
Michael Musil <infi.pc@gmail.com>
2021-12-01 14:37:23 +0100

    label

--- dist/runtime.js
+++ dist/runtime.js
@@ -10,31 +10,70 @@ exports.register = register;
 var PADDING = 6;
 if (typeof window !== "undefined") {
     // add style tag to head
-    //   const style = document.createElement("style");
-    //   style.innerHTML = `
-    //         [data-vispr-id]:hover {
-    //             outline: 1px solid red;
-    //         }
-    //     `;
-    //   document.head.appendChild(style);
+    var style = document.createElement("style");
+    style.innerHTML = "\n        #vispr-label {\n            cursor: pointer;\n            background-color: #ff0000;\n        }\n        #vispr-label:hover {\n            background-color: #aa3333;\n        }\n    ";
+    document.head.appendChild(style);
+    document.addEventListener("scroll", function () {
+        // hide layers when scrolling
+        var el = document.getElementById("vispr-layer");
+        if (!el) {
+            throw new Error("no layer found");
+        }
+        el.innerHTML = "";
+    });
     document.addEventListener("mouseover", function (e) {
         if (e.target) {
+            console.log(e.target);
+            if (
+            // @ts-ignore
+            e.target.id == "vispr-label" ||
+                // @ts-ignore
+                e.target.id == "vispr-label-wrapper") {
+                return;
+            }
             // @ts-ignore
             var found = e.target.closest("[data-vispr-id]");
             // add bouding box to the layer
             if (found) {
                 var _a = found.dataset.visprId.split("::"), filePath = _a[0], id = _a[1];
                 var data = dataByFilename[filePath];
-                if (data) {
+                var expData = data.expressions[id];
+                if (expData) {
                     var bbox = found.getBoundingClientRect();
                     var rect = document.createElement("div");
                     rect.style.position = "absolute";
                     rect.style.left = bbox.x - PADDING + "px";
                     rect.style.top = bbox.y - PADDING + "px";
-                    rect.style.width = (bbox.width + PADDING * 2) + "px";
-                    rect.style.height = (bbox.height + PADDING * 2) + "px";
+                    rect.style.width = bbox.width + PADDING * 2 + "px";
+                    rect.style.height = bbox.height + PADDING * 2 + "px";
                     rect.style.border = "2px solid #ff0000";
                     rect.style.borderRadius = "8px";
+                    var topPart = document.createElement("div");
+                    topPart.style.position = "absolute";
+                    topPart.style.display = "flex";
+                    topPart.style.justifyContent = "center";
+                    topPart.style.top = "-30px";
+                    topPart.style.left = "0px";
+                    topPart.style.width = "100%";
+                    rect.appendChild(topPart);
+                    var labelWrapper = document.createElement("div");
+                    labelWrapper.style.padding = "4px 8px 8px 8px";
+                    // labelWrapper.style.backgroundColor = "#00ff00";
+                    labelWrapper.style.pointerEvents = "auto";
+                    labelWrapper.id = "vispr-label-wrapper";
+                    topPart.appendChild(labelWrapper);
+                    var label = document.createElement("div");
+                    // label.style.backgroundColor = "#ff0000";
+                    label.style.color = "#fff";
+                    label.style.fontSize = "12px";
+                    label.style.fontWeight = "bold";
+                    label.style.textAlign = "center";
+                    label.style.padding = "2px 6px";
+                    label.style.borderRadius = "4px";
+                    label.style.fontFamily = "Helvetica, sans-serif, Arial";
+                    label.innerText = expData.name;
+                    label.id = "vispr-label";
+                    labelWrapper.appendChild(label);
                     var el = document.getElementById("vispr-layer");
                     if (!el) {
                         throw new Error("no layer found");
--- src/runtime.ts
+++ src/runtime.ts
@@ -5,22 +5,44 @@ export function register(input: any) {
   dataByFilename[input.filePath] = input;
 }
 
-const PADDING = 6
+const PADDING = 6;
 
 if (typeof window !== "undefined") {
   // add style tag to head
-//   const style = document.createElement("style");
-//   style.innerHTML = `
-//         [data-vispr-id]:hover {
-//             outline: 1px solid red;
-//         }
-//     `;
-//   document.head.appendChild(style);
+  const style = document.createElement("style");
+  style.innerHTML = `
+        #vispr-label {
+            cursor: pointer;
+            background-color: #ff0000;
+        }
+        #vispr-label:hover {
+            background-color: #aa3333;
+        }
+    `;
+  document.head.appendChild(style);
+
+  document.addEventListener("scroll", () => {
+    // hide layers when scrolling
+    const el = document.getElementById("vispr-layer");
+    if (!el) {
+      throw new Error("no layer found");
+    }
+    el.innerHTML = "";
+  });
 
   document.addEventListener(
     "mouseover",
     function (e) {
       if (e.target) {
+        console.log(e.target);
+        if (
+          // @ts-ignore
+          e.target.id == "vispr-label" ||
+          // @ts-ignore
+          e.target.id == "vispr-label-wrapper"
+        ) {
+          return;
+        }
         // @ts-ignore
         const found = e.target.closest("[data-vispr-id]");
         // add bouding box to the layer
@@ -28,24 +50,54 @@ if (typeof window !== "undefined") {
         if (found) {
           const [filePath, id] = found.dataset.visprId.split("::");
           const data = dataByFilename[filePath];
-
-          if (data) {
+          const expData = data.expressions[id];
+          if (expData) {
             const bbox = found.getBoundingClientRect();
             const rect = document.createElement("div");
             rect.style.position = "absolute";
             rect.style.left = bbox.x - PADDING + "px";
             rect.style.top = bbox.y - PADDING + "px";
-            rect.style.width = (bbox.width + PADDING * 2) + "px";
-            rect.style.height = (bbox.height + PADDING * 2) + "px";
+            rect.style.width = bbox.width + PADDING * 2 + "px";
+            rect.style.height = bbox.height + PADDING * 2 + "px";
             rect.style.border = "2px solid #ff0000";
             rect.style.borderRadius = "8px";
-            const el = document.getElementById("vispr-layer")
+
+            const topPart = document.createElement("div");
+            topPart.style.position = "absolute";
+            topPart.style.display = "flex";
+            topPart.style.justifyContent = "center";
+            topPart.style.top = "-30px";
+            topPart.style.left = "0px";
+            topPart.style.width = "100%";
+            rect.appendChild(topPart);
+
+            const labelWrapper = document.createElement("div");
+            labelWrapper.style.padding = "4px 8px 8px 8px";
+            // labelWrapper.style.backgroundColor = "#00ff00";
+            labelWrapper.style.pointerEvents = "auto";
+            labelWrapper.id = "vispr-label-wrapper";
+            topPart.appendChild(labelWrapper);
+
+            const label = document.createElement("div");
+            // label.style.backgroundColor = "#ff0000";
+            label.style.color = "#fff";
+            label.style.fontSize = "12px";
+            label.style.fontWeight = "bold";
+            label.style.textAlign = "center";
+            label.style.padding = "2px 6px";
+            label.style.borderRadius = "4px";
+            label.style.fontFamily = "Helvetica, sans-serif, Arial";
+            label.innerText = expData.name;
+            label.id = "vispr-label";
+            labelWrapper.appendChild(label);
+
+            const el = document.getElementById("vispr-layer");
             if (!el) {
-                throw new Error("no layer found")
+              throw new Error("no layer found");
             }
             el.innerHTML = "";
             el.appendChild(rect);
-            
+
             // document.body.childNodes = [rect]
           }
         }
