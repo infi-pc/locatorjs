16d2d5c21bdbe1c028118ef8c7f30334dfcc09d4
Michael Musil <infi.pc@gmail.com>
2021-12-26 14:27:53 +0100

    use CI branch name

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,20 +1,23 @@
 import "../styles/globals.css";
 import { setup as setupLocator } from "@locator/runtime";
 
-console.log("process.env.NODE_ENV:", process.env.NODE_ENV)
+const branchName = process.env.VERCEL_GIT_COMMIT_REF || "master";
 
 setupLocator(
   process.env.NODE_ENV === "production"
     ? {
+        // On production we have to enable it with cookies, on stagings it is just hidden
+        // defaultMode: process.env.VERCEL_ENV === "production" ? "disabled" : "hidden",
         defaultMode: "hidden",
         targets: {
           GitHub:
-            "https://www.github.com/infi-pc/locatorjs/blob/master/apps/web${filePath}#L${line}",
+            `https://www.github.com/infi-pc/locatorjs/blob/${branchName}/apps/web\${filePath}#L\${line}`,
           Editor:
-            "https://github.dev/infi-pc/locatorjs/blob/master/apps/web${filePath}#L${line}",
+            `https://github.dev/infi-pc/locatorjs/blob/${branchName}/apps/web\${filePath}#L\${line}`,
         },
       }
     : {
+        // Show initial setup to all devs in your team so they can choose their editor.
         defaultMode: "options",
       }
 );
