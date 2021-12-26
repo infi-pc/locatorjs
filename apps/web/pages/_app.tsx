4b9e7912256fe7cd43927cba3d9231ae557d0a99
Michael Musil <infi.pc@gmail.com>
2021-12-26 18:19:19 +0100

    web - icons

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals.css";
 import { setup as setupLocator } from "@locator/runtime";
+import Head from "next/head";
 
 const branchName = process.env.VERCEL_GIT_COMMIT_REF || "master";
 
@@ -10,10 +11,8 @@ setupLocator(
         // defaultMode: process.env.VERCEL_ENV === "production" ? "disabled" : "hidden",
         defaultMode: "hidden",
         targets: {
-          GitHub:
-            `https://www.github.com/infi-pc/locatorjs/blob/${branchName}/apps/web\${filePath}#L\${line}`,
-          Editor:
-            `https://github.dev/infi-pc/locatorjs/blob/${branchName}/apps/web\${filePath}#L\${line}`,
+          GitHub: `https://www.github.com/infi-pc/locatorjs/blob/${branchName}/apps/web\${filePath}#L\${line}`,
+          Editor: `https://github.dev/infi-pc/locatorjs/blob/${branchName}/apps/web\${filePath}#L\${line}`,
         },
       }
     : {
@@ -23,7 +22,16 @@ setupLocator(
 );
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+        <title>LocatorJS - click on any component to get to code.</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
