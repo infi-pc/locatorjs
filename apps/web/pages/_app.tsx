b936c2637acbf1dd3fbda5d66d09742284f9f28b
Michael Musil <infi.pc@gmail.com>
2021-12-27 15:02:06 +0100

    add smatlook to the website

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -28,12 +28,33 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
         <title>LocatorJS - click on any component to go to code.</title>
-        <meta name="description" content="A plugin for you dev stack that allows you to click trough from your app to it's code."></meta>
+        <meta
+          name="description"
+          content="A plugin for you dev stack that allows you to click trough from your app to it's code."
+        ></meta>
         <meta property="og:image" content="/preview.png"></meta>
         <meta name="twitter:card" content="summary_large_image"></meta>
         <meta name="twitter:site" content="@_michaelmusil"></meta>
-        <meta name="twitter:title" content="LocatorJS - click on any component to go to code."></meta>
-        <meta name="twitter:image" content="https://www.locatorjs.com/preview.png"></meta>
+        {/* <meta name="twitter:site" content="@_michaelmusil" />
+        <meta name="twitter:creator" content="@_michaelmusil" />    */}
+        <meta
+          name="twitter:title"
+          content="LocatorJS - click on any component to go to code."
+        ></meta>
+        <meta
+          name="twitter:image"
+          content="https://www.locatorjs.com/preview.png"
+        ></meta>
+        {process.env.NODE_ENV === "production" && (
+          <script type="text/javascript">
+            {`window.smartlook||(function(d) {
+    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+    c.charset='utf-8';c.src='https://rec.smartlook.com/recorder.js';h.appendChild(c);
+    })(document);
+    smartlook('init', 'f69eb3dd4cb7531a91e2657bfd2a509e8d23e8b4');`}
+          </script>
+        )}
       </Head>
       <Component {...pageProps} />
     </>
