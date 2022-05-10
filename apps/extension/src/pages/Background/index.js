64d7ee7eaa9be91311991545494d90b9efa82d88
Michael Musil <infi.pc@gmail.com>
2022-05-10 20:51:46 +0200

    prep

--- apps/extension/src/pages/Background/index.js
+++
@@ -1,2 +0,0 @@
-console.log('This is the background page.');
-console.log('Put the background scripts here.');
--- apps/extension/webpack.config.js
+++ apps/extension/webpack.config.js
@@ -43,7 +43,7 @@ const options = {
     newtab: path.join(__dirname, 'src', 'pages', 'Newtab', 'index.jsx'),
     options: path.join(__dirname, 'src', 'pages', 'Options', 'index.jsx'),
     popup: path.join(__dirname, 'src', 'pages', 'Popup', 'index.jsx'),
-    background: path.join(__dirname, 'src', 'pages', 'Background', 'index.js'),
+    background: path.join(__dirname, 'src', 'pages', 'Background', 'index.ts'),
     contentScript: path.join(__dirname, 'src', 'pages', 'Content', 'index.js'),
     devtools: path.join(__dirname, 'src', 'pages', 'Devtools', 'index.js'),
     panel: path.join(__dirname, 'src', 'pages', 'Panel', 'index.jsx'),
