7839924e97bd3241a79084d7f92b0f5b0468930e
Michael Musil <infi.pc@gmail.com>
2022-05-11 08:51:50 +0200

    add helper popover

--- apps/extension/src/pages/Popup/Popup.jsx
+++ apps/extension/src/pages/Popup/Popup.jsx
@@ -1,10 +1,24 @@
 import './Popup.css';
 
+const isMac =
+  typeof navigator !== "undefined" &&
+  navigator.platform.toUpperCase().indexOf("MAC") >= 0;
+const altTitle = isMac ? "⌥ Option" : "Alt";
+
 const Popup = () => {
   return (
     <div class="App">
       <header class="App-header">
-        <p>
+      <div>
+        This is pre-release version of the LocatorJS extension.
+        <br />
+        <b>Focus your app</b> (click on any surface) and do one of followings: 
+        <br />
+        <br />
+        <div class="locatorjs-line"><b>Press and hold <span class="locatorjs-key">${altTitle}</span>:</b> make boxes clickable on full surface</div>
+        <div class="locatorjs-line"><b><span class="locatorjs-key">${altTitle}</span> + <span class="locatorjs-key">D</span>:</b> hide/show LocatorJS panel</div>
+      </div>
+        {/* <p>
           Edit <code>src/pages/Popup/Popup.jsx</code> and save cool reload.
         </p>
         <a
@@ -14,7 +28,7 @@ const Popup = () => {
           rel="noopener noreferrer"
         >
           Learn Solid-JS!
-        </a>
+        </a> */}
       </header>
     </div>
   );
