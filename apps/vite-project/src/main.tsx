c85521c74d63a7d2e81f87ac0ec5ee86c208fab3
Michael Musil <infi.pc@gmail.com>
2022-05-16 08:42:50 +0200

    add vite project for testing

--- apps/vite-project/src/main.tsx
+++ apps/vite-project/src/main.tsx
@@ -1,13 +1,10 @@
-// TODO make import somehow universal for esmodule and compiled
-import "@locator/react-devtools-hook/src/autoInstallDevtoolsHook";
-import "@locator/runtime/src";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
