7f73bcce36f1aabe56164d6ed486201d60d8f845
Michael Musil <infi.pc@gmail.com>
2022-05-22 19:00:05 +0200

    clean from boilerplate

--- apps/extension/src/pages/Newtab/index.jsx
+++
@@ -1,8 +0,0 @@
-import { render } from 'solid-js/web';
-
-import Newtab from './Newtab';
-import './index.css';
-
-render(Newtab, window.document.querySelector('#app-container'));
-
-if (module.hot) module.hot.accept();
--- apps/extension/src/pages/Panel/index.jsx
+++
@@ -1,8 +0,0 @@
-import { render } from 'solid-js/web';
-
-import Panel from './Panel';
-import './index.css';
-
-render(Panel, window.document.querySelector('#app-container'));
-
-if (module.hot) module.hot.accept();
