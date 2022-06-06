1662dd14019bc995534873ffe76364a5f596f01e
Michael Musil <infi.pc@gmail.com>
2022-06-06 15:19:13 +0200

    fix chrome again (after fixing firefox)

--- apps/extension/src/browser.ts
+++ apps/extension/src/browser.ts
@@ -1,9 +1,12 @@
 // @ts-ignore
-let browserObj: typeof chrome = browser;
+let browserObj: typeof chrome;
 
 // @ts-ignore
 if (typeof browser === 'undefined') {
   browserObj = chrome;
+} else {
+  // @ts-ignore
+  browserObj = browser;
 }
 
 export default browserObj;
