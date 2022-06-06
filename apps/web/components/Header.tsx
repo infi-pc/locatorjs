07f93288dc0d2e69e57d5bf95d1358f9822d9396
Michael Musil <infi.pc@gmail.com>
2022-06-06 15:21:33 +0200

    chore(web): clean header buttons

--- apps/web/components/Header.tsx
+++ apps/web/components/Header.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import React from "react";
 import logo from "../public/logo-noborders3x.png";
-import GitHubButton from "react-next-github-btn";
+import { FaChrome, FaFirefox, FaGithub } from "react-icons/fa";
+import { extensionLink, useBrowser } from "./shared";
 
 function Header() {
+  const browser = useBrowser();
   return (
     <header className="text-gray-600 body-font dark:text-gray-400 dark:bg-gray-900">
       <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
@@ -16,37 +18,29 @@ function Header() {
             alt="LocatorJS logo"
           ></Image>
         </a>
-        <nav className="flex flex-wrap items-center justify-center gap-1 text-base md:ml-auto">
-          {/* <a href="" className="mr-5 hover:text-gray-900 dark:hover:text-white">
-            First Link
+        <nav className="flex flex-wrap items-center justify-center gap-2 text-base md:ml-auto">
+          <a
+            href={
+              browser === "firefox"
+                ? extensionLink.firefox
+                : extensionLink.chrome
+            }
+            className="flex items-center gap-1 px-3 py-1 text-base border-0 rounded bg-slate-100 focus:outline-none hover:bg-slate-200"
+          >
+            {browser === "firefox" ? (
+              <FaFirefox></FaFirefox>
+            ) : (
+              <FaChrome></FaChrome>
+            )}{" "}
+            Get Extension
           </a>
-          <a href="" className="mr-5 hover:text-gray-900 dark:hover:text-white">
-            Second Link
-          </a>*/}
-          {/* <a
-            href="https://chrome.google.com/webstore/detail/locatorjs/npbfdllefekhdplbkdigpncggmojpefi"
-            className="mr-5 hover:text-gray-900 dark:hover:text-white"
-          >
-            Get Chrome Extension
-          </a> */}
 
-          {/* <a
-            href="https://chrome.google.com/webstore/detail/locatorjs/npbfdllefekhdplbkdigpncggmojpefi"
-            className="flex px-3 py-1 text-base text-white border-0 rounded bg-primary-500 focus:outline-none hover:bg-primary-600"
+          <a
+            href="https://github.com/infi-pc/locatorjs"
+            className="flex items-center gap-1 px-3 py-1 text-base border-0 rounded bg-slate-100 focus:outline-none hover:bg-slate-200"
           >
-            Get Chrome Extension
-          </a> */}
-
-          <div className="mt-2">
-            <GitHubButton
-              href="https://github.com/infi-pc/locatorjs"
-              data-icon="octicon-star"
-              data-size="large"
-              aria-label="Star infi-pc/locatorjs on GitHub"
-            >
-              Star
-            </GitHubButton>
-          </div>
+            <FaGithub></FaGithub> GitHub
+          </a>
         </nav>
       </div>
     </header>
--- apps/web/components/ReadMore.tsx
+++ apps/web/components/ReadMore.tsx
@@ -11,10 +11,10 @@ function ReadMore() {
           Read more
         </h3>
         <p className="text-center">Get more info on other sites.</p>
-        <div className="flex justify-center gap-4 p-4 mt-4">
+        <div className="flex flex-col items-center justify-center gap-4 p-4 mt-4 sm:flex-row">
           <a
             href="https://dev.to/michael_vp/introducing-locatorjs-click-on-react-component-to-get-to-its-code-2oj0"
-            className="overflow-hidden transition-shadow shadow-xl bg-slate-50 xl:w-1/4 md:w-1/2 rounded-xl hover:shadow-2xl"
+            className="overflow-hidden transition-shadow shadow-xl bg-slate-50 xl:w-1/4 md:w-1/2 rounded-xl hover:shadow-2xl max-w-[400px]"
           >
             <Image
               className="object-cover object-center w-full h-40 mb-6 rounded"
@@ -40,7 +40,7 @@ function ReadMore() {
           </a>
           <a
             href="https://medium.com/@infi.cz/how-i-increased-my-react-development-productivity-by-8-6420c42f4022"
-            className="overflow-hidden transition-shadow shadow-xl bg-slate-50 xl:w-1/4 md:w-1/2 rounded-xl hover:shadow-2xl"
+            className="overflow-hidden transition-shadow shadow-xl bg-slate-50 xl:w-1/4 md:w-1/2 rounded-xl hover:shadow-2xl max-w-[400px]"
           >
             <Image
               className="object-cover object-center w-full h-40 mb-6 rounded"
