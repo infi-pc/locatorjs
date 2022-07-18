16b688e72441158fc8c310df97b7baa215ab97f4
Michael Musil <infi.pc@gmail.com>
2022-07-18 20:42:57 +0200

    use localstorage

--- packages/runtime/src/linkTemplateUrl.ts
+++ packages/runtime/src/linkTemplateUrl.ts
@@ -1,24 +1,12 @@
 import { allTargets } from "@locator/shared";
 
-function setCookie(name: string, value: string) {
-  document.cookie = name + "=" + (value || "") + "; path=/";
-}
-
 export function setTemplate(linkOrTemplate: string) {
-  setCookie("LOCATOR_CUSTOM_LINK", linkOrTemplate);
-}
-
-function getCookie(name: string) {
-  if (typeof document === "undefined") {
-    return;
-  }
-  const match = document.cookie.match(new RegExp("(^| )" + name + "=([^;]+)"));
-  if (match) return match[2];
+  localStorage.setItem("LOCATOR_CUSTOM_LINK", linkOrTemplate);
 }
 
 export const getLinkTypeOrTemplate = () =>
   document.documentElement.dataset.locatorTarget ||
-  getCookie("LOCATOR_CUSTOM_LINK") ||
+  localStorage.getItem("LOCATOR_CUSTOM_LINK") ||
   "vscode";
 
 export const linkTemplate = () => allTargets[getLinkTypeOrTemplate()];
