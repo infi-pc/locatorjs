84f8fbbd1e3f047f6a06036bf7092fcbbf660fb3
Michael Musil <infi.pc@gmail.com>
2022-07-19 08:54:10 +0200

    p

--- apps/vite-react-project/src/main.tsx
+++ apps/vite-react-project/src/main.tsx
@@ -1,10 +1,12 @@
-// TODO make import somehow universal for esmodule and compiled
-import "@locator/react-devtools-hook/src/autoInstallDevtoolsHook";
-import "@locator/runtime/src";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { setup } from "@locator/runtime";
+
+setup({
+  adapter: "jsx",
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- apps/vite-react-project/vite.config.ts
+++ apps/vite-react-project/vite.config.ts
@@ -3,5 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        // plugins: ["@locator/babel-jsx/dist"],
+      },
+    }),
+  ],
 });
