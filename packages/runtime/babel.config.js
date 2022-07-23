6374a24649fede47e302452995e7dbf05d4478d3
Michael Musil <infi.pc@gmail.com>
2022-07-23 20:49:56 +0200

    fix test

--- packages/runtime/babel.config.js
+++ packages/runtime/babel.config.js
@@ -1,7 +1,14 @@
-module.exports = {
-  presets: [
-    ["@babel/preset-env", { targets: { node: "current" }, modules: false }],
-    "@babel/preset-typescript",
-    "solid",
-  ],
+// eslint-disable-next-line no-undef
+module.exports = (api) => {
+  const isTest = api.env("test");
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        { targets: { node: "current" }, modules: isTest ? undefined : false },
+      ],
+      "@babel/preset-typescript",
+      "solid",
+    ],
+  };
 };
