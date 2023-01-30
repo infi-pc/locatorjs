5ec05976e8cb730f294694a4e24e6e9b01dc80ea
Michael Musil <infi.pc@gmail.com>
2023-01-30 19:33:09 +0100

    add clean CRA

---
+++ apps/cra-clean-project/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          eee a ssssdf ertx xe fsf lll lll pp jjjj xxx sss ppp llll Edit{" "}
+          <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React c c t l o
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
