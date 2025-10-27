0b6021fcf53d7d7e113f21c8e84a3750ff8d8d8c
Michael Musil <infi.pc@gmail.com>
2025-10-27 12:41:49 +0100

    remove unnecesary stuff

--- apps/next-16/app/page.tsx
+++ apps/next-16/app/page.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
         <div className={styles.intro}>
           <h1>To get started, edit the page.tsx file.</h1>
           <p>
@@ -42,13 +33,6 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
             Deploy Now
           </a>
           <a
